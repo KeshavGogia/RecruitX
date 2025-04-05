b5764164453c584ad2e2ee3f6f1fe1ddb8daa775
Keshav <keshavgogia57@gmail.com>
2025-04-05 19:02:44 +0530

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+import Login from './pages/Login';
+import Landing from './pages/Landing';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
