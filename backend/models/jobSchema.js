d3402087770096a0c618ee0ef26e033a793aaa5f
Keshav <keshavgogia57@gmail.com>
2025-04-05 16:13:54 +0530

    Update jobSchema.js

--- backend/models/jobSchema.js
+++ backend/models/jobSchema.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import { v4 as uuidv4 } from "uuid";
 
 const jobSchema = new mongoose.Schema({
     title: { type: String, required: true, minLength: [3, "Title must be at least 3 characters long"] },
@@ -8,7 +7,7 @@ const jobSchema = new mongoose.Schema({
     location: { type: String, required: true },
     salary: { type: Number, required: true },
     recruiter: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    jobNumber: { type: String, unique: true, required: true, default: uuidv4 }
+    jobNumber: { type: String, unique: true, required: true }
 }, { timestamps: true });
 
 export const Job = mongoose.model("Job", jobSchema);
