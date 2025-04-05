44b9243e3c60a987eca25b1aaabcc08d60e777c9
Keshav <keshavgogia57@gmail.com>
2025-04-05 16:13:57 +0530

    Update applicationSchema.js

--- backend/models/applicationSchema.js
+++ backend/models/applicationSchema.js
@@ -4,8 +4,12 @@ import mongoose from "mongoose";
 const applicationSchema = new mongoose.Schema({
     job: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
     candidate: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true, validate: [validator.isEmail, "Enter a valid email"] },
+    phone: { type: String, required: true },
     resumeURL: { type: String, required: true },
+    jobNumber: { type: String, required: true },
 }, { timestamps: true });
 
 export const Application = mongoose.model("Application", applicationSchema);
