a46cd5aeec55de0d59eabdab7190df03f7c7504b
Keshav <keshavgogia57@gmail.com>
2025-04-07 19:06:56 +0530

    Update jobRouter.js

--- backend/router/jobRouter.js
+++ backend/router/jobRouter.js
@@ -1,14 +1,15 @@
 import express from "express";
 import { upload } from "../middleware/upload.js";
-import { createJob, getAllJobs, applyForJob, getApplicants, deleteJob } from "../controller/jobController.js";
+import { createJob, getAllJobs, applyForJob, getApplicants, deleteJob , getJob} from "../controller/jobController.js";
 import { isRecruiterAuthenticated, isCandidateAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/create", isRecruiterAuthenticated, createJob);
-router.get("/listings", getAllJobs);
+router.get("/listings",isRecruiterAuthenticated, getAllJobs);
 router.post("/apply/:jobNumber", isCandidateAuthenticated,upload.single("resume"),applyForJob);
 router.get("/applicants/:jobNumber", isRecruiterAuthenticated, getApplicants);
 router.delete("/delete/:jobNumber", isRecruiterAuthenticated, deleteJob);
+router.get("/details/:jobNumber",isRecruiterAuthenticated,getJob);
 
 export default router;
